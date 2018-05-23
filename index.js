1e5ab011f0dd385cd0e090ece1f48e27b39d32be
William Chabla <w_chabla@hotmail.com>
2018-05-23 01:11:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
+  if (string === "I love you, Grandma."){
+    return "I love you, too.";
+  }
+  
   if (string === string.toLowerCase()){
     return "I can't hear you!";
   }
