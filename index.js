4d074294bebe98e43541c2a705c22f4dbc859fcb
William Chabla <w_chabla@hotmail.com>
2018-05-23 01:09:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,12 @@ function logShout(string){
   console.log (string.toUpperCase());
 }
 
+function logWhisper(string){
+  console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string){
+  if (string === string.toLowerCase()){
+    return "I can't hear you!";
+  }
+}
